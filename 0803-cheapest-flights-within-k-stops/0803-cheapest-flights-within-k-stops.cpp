9a29a4f628bc5e8bb8fdb92cfa8b0050bece6153
justgrego <78941719+justgrego@users.noreply.github.com>
2023-05-10 14:10:41 -0700

    Time: 19 ms (90.75%), Space: 13.7 MB (53.62%) - LeetHub

---
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        vector<pair<int,int>> adj[n];
+        for(auto it : flights) {
+            adj[it[0]].push_back({it[1],it[2]});
+        }
+        
+        vector<int> dist(n,1e9);
+        dist[src] = 0;
+        
+        // {stops{node,cost}}
+        queue<pair<int,pair<int,int>>> q;
+        q.push({0,{src,0}});
+        
+        while(!q.empty()){
+            int stops = q.front().first;
+            int node = q.front().second.first;
+            int cost = q.front().second.second;
+            q.pop();
+            
+            if(stops > k)
+                break;
+            
+            for(auto it : adj[node]) {
+                int adjNode = it.first;
+                int adjWt = it.second;
+                
+                if(cost + adjWt < dist[adjNode]) {
+                    dist[adjNode] = cost + adjWt;
+                    q.push({stops+1,{adjNode,dist[adjNode]}});
+                }
+            }
+        }
+        
+        
+        if(dist[dst] == 1e9)
+            return -1;
+        return dist[dst];
+    }
+};
