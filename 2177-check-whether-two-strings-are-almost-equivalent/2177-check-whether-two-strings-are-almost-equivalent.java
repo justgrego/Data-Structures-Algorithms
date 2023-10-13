893f8dd426413565171def654967c1144cc0c11a
justgrego <78941719+justgrego@users.noreply.github.com>
2023-10-12 17:58:08 -0700

    Time: 7 ms (5.29%), Space: 41 MB (20.28%) - LeetHub

---
+++ 2177-check-whether-two-strings-are-almost-equivalent/2177-check-whether-two-strings-are-almost-equivalent.java
@@ -0,0 +1,46 @@
+class Solution {
+    public boolean checkAlmostEquivalent(String word1, String word2) {
+        int counter = 0;
+        HashMap<Character, Integer> map = new HashMap<>();
+        HashMap<Character, Integer> map2 = new HashMap<>();
+        for (int i = 0; i < word1.length(); i++) {
+            if (map.containsKey(word1.charAt(i))) {
+                map.put(word1.charAt(i), map.get(word1.charAt(i)) + 1);
+            }
+            else {
+                map.put(word1.charAt(i), 1);
+            }
+
+            if (map2.containsKey(word2.charAt(i))) {
+                map2.put(word2.charAt(i), map2.get(word2.charAt(i)) + 1);
+            }
+            else {
+                map2.put(word2.charAt(i), 1);
+            }
+        }
+        int max = 0;
+        for (Map.Entry<Character, Integer> e : map.entrySet()) {
+            if (map2.containsKey(e.getKey())) {
+                max = Math.max(max, Math.abs(e.getValue() - map2.get(e.getKey())));
+            }
+            else {
+                System.out.println("her");
+                max = Math.max(max, e.getValue());
+                
+            }
+        }
+
+        for (Map.Entry<Character, Integer> e : map2.entrySet()) {
+            if (map.containsKey(e.getKey())) {
+                max = Math.max(max, Math.abs(e.getValue() - map.get(e.getKey())));
+            }
+            else {
+                System.out.println("her");
+                max = Math.max(max, e.getValue());
+                
+            }
+        }
+       
+        return max <= 3;
+    }
+}
