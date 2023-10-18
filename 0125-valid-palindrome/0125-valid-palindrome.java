3f10f24315443fae79c9e253875fd5dfb98d6806
justgrego <78941719+justgrego@users.noreply.github.com>
2023-10-17 17:50:32 -0700

    Time: 27 ms (17.04%), Space: 43.2 MB (56.8%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+
+        s = s.toLowerCase();
+        String filteredText = s.replaceAll("[^a-zA-Z0-9]", "");
+        System.out.println(filteredText);
+        int len = filteredText.length() - 1; 
+        for (int i = 0; i < filteredText.length() / 2; i++) {
+            if (filteredText.charAt(i) == filteredText.charAt(len)) {
+                len--;
+                continue;
+            }
+            else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
